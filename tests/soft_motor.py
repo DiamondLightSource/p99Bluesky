e5a65b3cd03f0b04617c166f0cb876bacd61af52
Relm-Arrowny <raymond.fan@diamond.ac.uk>
2024-07-11 10:24:15 +0000

    making softMotor flyable

--- tests/soft_motor.py
+++ tests/soft_motor.py
@@ -1,5 +1,4 @@
 import asyncio
-from collections.abc import Callable
 
 from bluesky.protocols import Movable, Stoppable
 from ophyd_async.core import (
@@ -9,7 +8,7 @@ from ophyd_async.core import (
     StandardReadable,
     WatchableAsyncStatus,
 )
-from ophyd_async.core.signal import AsyncStatus, SignalR, T, observe_value, wait_for_value
+from ophyd_async.core.signal import AsyncStatus, observe_value
 from ophyd_async.core.utils import (
     DEFAULT_TIMEOUT,
     CalculatableTimeout,
@@ -17,6 +16,39 @@ from ophyd_async.core.utils import (
     WatcherUpdate,
 )
 from ophyd_async.epics.signal import epics_signal_r, epics_signal_rw, epics_signal_x
+from pydantic import BaseModel, Field
+
+
+class MotorLimitsException(Exception):
+    pass
+
+
+class InvalidFlyMotorException(Exception):
+    pass
+
+
+DEFAULT_MOTOR_FLY_TIMEOUT = 60
+DEFAULT_WATCHER_UPDATE_FREQUENCY = 0.2
+
+
+class FlyMotorInfo(BaseModel):
+    """Minimal set of information required to fly a motor:"""
+
+    #: Absolute position of the motor once it finishes accelerating to desired
+    #: velocity, in motor EGUs
+    start_position: float = Field(frozen=True)
+
+    #: Absolute position of the motor once it begins decelerating from desired
+    #: velocity, in EGUs
+    end_position: float = Field(frozen=True)
+
+    #: Time taken for the motor to get from start_position to end_position, excluding
+    #: run-up and run-down, in seconds.
+    time_for_move: float = Field(frozen=True, gt=0)
+
+    #: Maximum time for the complete motor move, including run up and run down.
+    #: Defaults to `time_for_move` + run up and run down times + 10s.
+    timeout: CalculatableTimeout = Field(frozen=True, default=CalculateTimeout)
 
 
 class SoftThreeAxisStage(Device):
@@ -80,6 +112,16 @@ class SoftMotor(StandardReadable, Movable, Stoppable):
         self.motor_stop = epics_signal_x(prefix + "STOP")
         # Whether set() should complete successfully or not
         self._set_success = True
+
+        # end_position of a fly move, with run_up_distance added on.
+        self._fly_completed_position: float | None = None
+
+        # Set on kickoff(), complete when motor reaches self._fly_completed_position
+        self._fly_status: WatchableAsyncStatus | None = None
+
+        # Set during prepare
+        self._fly_timeout: CalculatableTimeout | None = CalculateTimeout
+
         super().__init__(name=name)
 
     def set_name(self, name: str):
@@ -88,14 +130,42 @@ class SoftMotor(StandardReadable, Movable, Stoppable):
         self.user_readback.set_name(name)
 
     @AsyncStatus.wrap
-    async def wait_for_value_with_status(
-        self,
-        signal: SignalR[T],
-        match: T | Callable[[T], bool],
-        timeout: float | None,
-    ):
-        """wrap wait for value so it return an asyncStatus"""
-        await wait_for_value(signal, match, timeout)
+    async def prepare(self, value: FlyMotorInfo):
+        """Calculate required velocity and run-up distance, then if motor limits aren't
+        breached, move to start position minus run-up distance"""
+
+        self._fly_timeout = value.timeout
+
+        # Velocity, at which motor travels from start_position to end_position, in motor
+        # egu/s.
+        fly_velocity = await self._prepare_velocity(
+            value.start_position,
+            value.end_position,
+            value.time_for_move,
+        )
+
+        # start_position with run_up_distance added on.
+        fly_prepared_position = await self._prepare_motor_path(
+            abs(fly_velocity), value.start_position, value.end_position
+        )
+
+        await self.set(fly_prepared_position)
+
+    @AsyncStatus.wrap
+    async def kickoff(self):
+        """Begin moving motor from prepared position to final position."""
+        assert (
+            self._fly_completed_position
+        ), "Motor must be prepared before attempting to kickoff"
+
+        self._fly_status = self.set(
+            self._fly_completed_position, timeout=self._fly_timeout
+        )
+
+    def complete(self) -> WatchableAsyncStatus:
+        """Mark as complete once motor reaches completed position."""
+        assert self._fly_status, "kickoff not called"
+        return self._fly_status
 
     @WatchableAsyncStatus.wrap
     async def set(self, value: float, timeout: CalculatableTimeout = CalculateTimeout):
@@ -120,12 +190,7 @@ class SoftMotor(StandardReadable, Movable, Stoppable):
                 + 2 * acceleration_time
                 + DEFAULT_TIMEOUT
             )
-        # modified to actually wait for set point to be set
-        await self.user_setpoint.set(value, wait=True, timeout=timeout)
-        # changed this so that the watcher keep going until the motor is stopped
-        move_status = self.wait_for_value_with_status(
-            self.motor_done_move, True, timeout=None
-        )
+        move_status = self.user_setpoint.set(value, wait=True, timeout=timeout)
         async for current_position in observe_value(
             self.user_readback, done_status=move_status
         ):
@@ -145,5 +210,48 @@ class SoftMotor(StandardReadable, Movable, Stoppable):
         # Put with completion will never complete as we are waiting for completion on
         # the move above, so need to pass wait=False
         await self.motor_stop.trigger(wait=False)
-        # Trigger any callbacks
-        # await self.user_setpoint.set(await self.user_readback.get_value())
+
+    async def _prepare_velocity(
+        self, start_position: float, end_position: float, time_for_move: float
+    ) -> float:
+        fly_velocity = (start_position - end_position) / time_for_move
+        max_speed, egu = await asyncio.gather(
+            self.max_velocity.get_value(), self.motor_egu.get_value()
+        )
+        if abs(fly_velocity) > max_speed:
+            raise MotorLimitsException(
+                f"Motor speed of {abs(fly_velocity)} {egu}/s was requested for a motor "
+                f" with max speed of {max_speed} {egu}/s"
+            )
+        await self.velocity.set(abs(fly_velocity))
+        return fly_velocity
+
+    async def _prepare_motor_path(
+        self, fly_velocity: float, start_position: float, end_position: float
+    ) -> float:
+        # Distance required for motor to accelerate from stationary to fly_velocity, and
+        # distance required for motor to decelerate from fly_velocity to stationary
+        run_up_distance = (await self.acceleration_time.get_value()) * fly_velocity
+
+        self._fly_completed_position = end_position + run_up_distance
+
+        # Prepared position not used after prepare, so no need to store in self
+        fly_prepared_position = start_position - run_up_distance
+
+        motor_lower_limit, motor_upper_limit, egu = await asyncio.gather(
+            self.low_limit_travel.get_value(),
+            self.high_limit_travel.get_value(),
+            self.motor_egu.get_value(),
+        )
+
+        if (
+            not motor_upper_limit >= fly_prepared_position >= motor_lower_limit
+            or not motor_upper_limit >= self._fly_completed_position >= motor_lower_limit
+        ):
+            raise MotorLimitsException(
+                f"Motor trajectory for requested fly is from "
+                f"{fly_prepared_position}{egu} to "
+                f"{self._fly_completed_position}{egu} but motor limits are "
+                f"{motor_lower_limit}{egu} <= x <= {motor_upper_limit}{egu} "
+            )
+        return fly_prepared_position
