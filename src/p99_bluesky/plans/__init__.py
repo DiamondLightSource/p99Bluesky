9b61e2aed8b825c84b03a94a1e8123377ca4c275
Raymond Fan <ferimons@hotmail.com>
2024-10-08 14:10:45 +0100

    50 grid scan will not load into blueapi (#54)
    * removing callable in plan

--- src/p99_bluesky/plans/__init__.py
+++ src/p99_bluesky/plans/__init__.py
@@ -1,10 +1,11 @@
 from .ad_plans import takeImg, tiggerImg
-from .stxm import fast_scan_grid, stxm_fast  # , stxm_step
+from .stxm import fast_scan_grid, stxm_fast, stxm_step
 
 __all__ = [
     "takeImg",
     "tiggerImg",
     "fast_scan_grid",
     "stxm_fast",
+    "stxm_step",
 ]
 # the gird scan is bluesky si triggering pydantic error and crashing blueapi
